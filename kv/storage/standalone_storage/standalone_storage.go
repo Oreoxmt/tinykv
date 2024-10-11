8829c36be3c04d54bbf7596e8fc8fd15385ec678
Aolin <aolinz@outlook.com>
2024-10-11 20:59:53 +0800

    Revert "implement standalone storage engine (Write) using badger/v4"
    This reverts commit f93dc5d7b9aca471df991b93376da9b8817329cb.

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,17 +1,15 @@
 package standalone_storage
 
 import (
-	badger "github.com/dgraph-io/badger/v4"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
-	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	db *badger.DB
+	// Your Data Here (1).
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
@@ -20,20 +18,12 @@ func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 }
 
 func (s *StandAloneStorage) Start() error {
-	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	s.db = db
+	// Your Code Here (1).
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
-	err := s.db.Close()
-	if err != nil {
-		return err
-	}
-	s.db = nil
+	// Your Code Here (1).
 	return nil
 }
 
@@ -43,27 +33,6 @@ func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader,
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Provide a way that applies a series of modifications to the inner state, which is, in this situation, a badger instance.
-	err := s.db.Update(func(txn *badger.Txn) error {
-		for _, pair := range batch {
-			key := pair.Key()
-			val := pair.Value()
-			if val == nil {
-				err := txn.Delete(key)
-				if err != nil {
-					return err
-				}
-			} else {
-				err := txn.Set(pair.Key(), pair.Value())
-				if err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return err
-	}
+	// Your Code Here (1).
 	return nil
 }
